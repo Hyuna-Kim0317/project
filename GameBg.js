76b442f8efd174c8ea053d795b2682fae9a93db9
Hyuna-Kim0317 <november27blue@gmail.com>
2023-06-15 11:20:07 +0900

    맵 인스턴스 생성

---
+++ GameBg.js
@@ -0,0 +1,24 @@
+class GameBg{
+    constructor(container, width, height,x,y,velX,velY){
+        this.container=container;
+        this.div;
+        this.width=width;
+        this.height=height;
+        this.x=x;
+        this.y=y;
+        this.velX=velX;
+        this.velY=velY;
+
+        //스타일적용
+        this.div=document.createElement("div");
+        this.div.src=this.src;
+        this.div.style.width=this.width+"px";
+        this.div.style.height=this.height+"px";
+
+        this.div.style.position="absolute";
+        this.div.style.left=this.x+"px";
+        this.div.style.top=this.y+"px";
+
+        this.container.appendChild(this.div);
+    }
+}
